ff7d57a0c08ba656ec50eef40e87ba8bd1948f8c
i-am-wololo <35537849+i-am-wololo@users.noreply.github.com>
2025-11-21 15:14:41 +0100

    Update ex2.c

--- tp7/src/ex2.c
+++ tp7/src/ex2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
 	char* name;
@@ -7,14 +8,16 @@ typedef struct {
 	unsigned short age;
 } Personne;
 
-typedef struct {
+typedef struct Voisin Voisin;
+
+struct Voisin {
 	Personne* pers;
 	Voisin* prec;
 	Voisin* suivant;
+};
 
-} Voisin;
 
-typedef struct {
+typedef struct{
 	Voisin* first;
 	Voisin* last;
 } Rue;
@@ -29,17 +32,52 @@ Personne* lirePersonne(){
 	return person;
 };
 
-Rue* Population() {
-	Rue* rue = malloc(sizeof(Rue));
-	Voisin* voisin = malloc(sizeof(voisin));
+Voisin* creerVoisin(){
+	Voisin* voisin = (Voisin*)malloc(sizeof(Voisin));
+	voisin->prec = NULL;
+	voisin->suivant = NULL;
 	voisin->pers = lirePersonne();
-	rue->first = voisin;
-	printf("continuer? [y/n]\n");
-	char* reponse;
+	return voisin;
+}
+
+Rue* Population(){
+	Rue* rue = (Rue*)malloc(sizeof(Rue));
+	rue->last = creerVoisin();
+	Voisin* voisin = rue->last;
+
+	printf("continue? [y/n]\n");
+	getchar();
+	char reponse[10];
 	scanf("%s", reponse);
-	while (reponse == "yes") {
-		printf("continue? [y/n]");
-		scanf("%s", response);
-	}
+	printf("reponse donné: %s", reponse);
+	while (strcmp(reponse,"yes") == 0) {
+		voisin->prec = creerVoisin();
+		voisin = voisin->prec;
+		getchar();
+		printf("continue? [y/n]\n");
+		scanf("%s", reponse);
+		printf("\n");
+	};
+	rue->first = voisin;
+	return rue;
 };
 
+int voisinsAvant(Voisin* v){
+	if (v == NULL) return -1;
+	Voisin* voisin = v;
+	int i = 0;
+	while (voisin->prec != NULL) {
+		voisin = voisin->prec;
+		i++;
+	}
+	return i;
+}
+
+int voisinsApres(Voisin* v);
+
+
+int main(){
+	Rue* rue = Population();
+	Voisin* firstvoisin = 	
+	return 0;
+}
